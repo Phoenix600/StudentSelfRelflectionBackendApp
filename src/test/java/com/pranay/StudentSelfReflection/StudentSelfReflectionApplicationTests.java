f0e2a0c48885b5d1b4205948e674354b8319d076
pranay ramteke <pranayramteke613@gmail.com>
2025-04-17 16:40:08 +0530

    Getting Started With StudentSelfReflection Backend Project

---
+++ src/test/java/com/pranay/StudentSelfReflection/StudentSelfReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.StudentSelfReflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSelfReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
