6a3ef513a561b2ee75966dd1be6c042fbb00bc0c
pranay ramteke <pranayramteke613@gmail.com>
2025-05-09 16:50:20 +0530

    Add Course API For Admin

--- src/main/java/com/pranay/StudentSelfReflection/controller/AdminController.java
+++ src/main/java/com/pranay/StudentSelfReflection/controller/AdminController.java
@@ -1,5 +1,12 @@
 package com.pranay.StudentSelfReflection.controller;
 
+import com.pranay.StudentSelfReflection.model.Course;
+import com.pranay.StudentSelfReflection.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin/")
 public class AdminController
 {
-
+	
+	@Autowired
+	CourseService courseService;
+	
+	// POST http://localhost:8080/api/v1/admin/create-course
+	@PostMapping("/create-course")
+	public ResponseEntity<Course> createCourse(@RequestBody Course course)
+	{
+		Course courseResponse = courseService.saveCourse(course);
+		return new ResponseEntity<Course>(courseResponse, HttpStatus.CREATED);
+	}
 }
---
+++ src/main/java/com/pranay/StudentSelfReflection/services/CourseService.java
@@ -0,0 +1,51 @@
+package com.pranay.StudentSelfReflection.services;
+
+import com.pranay.StudentSelfReflection.model.Course;
+import com.pranay.StudentSelfReflection.model.Topic;
+import com.pranay.StudentSelfReflection.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Set;
+
+/**
+ * File Name: CourseServices.java
+ * Entity: CourseServices
+ * Package: com.pranay.StudentSelfReflection.services
+ * Author: pranayramteke
+ * Date: 09/05/25
+ * Description:
+ */
+
+@Service
+public class CourseService
+{
+	@Autowired
+	CourseRepository courseRepository;
+	
+	public Course saveCourse(Course course)
+	{
+		// TimeStamp Before The Actual Operation  (@Before)
+		Instant startTime = Instant.now();
+		System.out.println("saveCourse() method started at : " + startTime);
+		
+		// Actual Operation (@Around)
+		Course savedCourse =  courseRepository.save(course);
+		
+		Set<Topic> topics =  savedCourse.getTopics();
+		for(Topic x: topics)
+		{
+			x.setCourse(savedCourse);
+		}
+		
+		// TimeStamp After The Actual Operation (@After)
+		Instant endTime = Instant.now(); // 35 ms
+		System.out.println("saveCourse() method finished at : " + endTime);
+		System.out.println("saveCousre() method time elapsed : " + Duration.between(startTime, endTime));
+		
+		return savedCourse;
+		
+	}
+}
