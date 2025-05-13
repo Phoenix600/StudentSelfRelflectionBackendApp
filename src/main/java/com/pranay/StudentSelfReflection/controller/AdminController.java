53462b112110160d10bda81927e1274c4fed20d1
Root <s2p@gmail.com>
2025-05-13 03:42:04 -0700

    Added UpdateCourse And UpdateTopic

--- src/main/java/com/pranay/StudentSelfReflection/controller/AdminController.java
+++ src/main/java/com/pranay/StudentSelfReflection/controller/AdminController.java
@@ -1,14 +1,12 @@
 package com.pranay.StudentSelfReflection.controller;
 
 import com.pranay.StudentSelfReflection.model.Course;
+import com.pranay.StudentSelfReflection.model.Topic;
 import com.pranay.StudentSelfReflection.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * File Name: AdminController.java
@@ -33,5 +31,21 @@ public class AdminController
 	{
 		Course courseResponse = courseService.saveCourse(course);
 		return new ResponseEntity<Course>(courseResponse, HttpStatus.CREATED);
+	}
+
+	// PATCH http://localhost:8080/api/v1/admin/update-course/1
+	@PatchMapping("/update-course/{id}")
+	public ResponseEntity<Course> updateCourseDetails(@PathVariable("id") Long id,@RequestBody Course course)
+	{
+		Course courseResponse = courseService.updateCourse(id,course);
+		return new ResponseEntity<Course>(courseResponse, HttpStatus.OK);
+	}
+
+	// PATCH http://localhost:8080/api/v1/admin/update-topic?courseId={id}&topicId={id}
+	@PatchMapping("/update-topic")
+	public ResponseEntity<Course> updateTopicDetails(@RequestParam("courseId") Long courseId, @RequestParam("topicId") Long topicId, @RequestBody Topic topic)
+	{
+		Course courseResponse = courseService.updateCourseTopicByTopicId(courseId, topicId, topic);
+		return new ResponseEntity<Course>(courseResponse, HttpStatus.OK);
 	}
 }
--- src/main/java/com/pranay/StudentSelfReflection/services/CourseService.java
+++ src/main/java/com/pranay/StudentSelfReflection/services/CourseService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -47,5 +48,92 @@ public class CourseService
 		
 		return savedCourse;
 		
+	}
+
+	public Course updateCourse(Long id,Course course)
+	{
+		// TimeStamp Before The Actual Operation  (@Before)
+		Instant startTime = Instant.now();
+		System.out.println("updateCourse() method started at : " + startTime);
+
+		// Starts Here Code
+		Course upadtedCourse = null;
+		// Fetch The Course With courseId
+		Optional<Course> courseOptional = courseRepository.findById(id);
+
+		// Then Write The Code For Update
+		if(courseOptional.isPresent())
+		{
+			Course fetchedCourse = courseOptional.get();
+
+			if(!course.getCourseName().isEmpty())
+			{
+				String courseName = course.getCourseName();
+				fetchedCourse.setCourseName(courseName);
+			}
+
+			if(!course.getDescription().isEmpty())
+			{
+				String courseDescription = course.getDescription();
+				fetchedCourse.setDescription(courseDescription);
+			}
+
+			upadtedCourse =  courseRepository.save(fetchedCourse);
+
+		}
+		// End Of Code
+
+		// TimeStamp After The Actual Operation (@After)
+		Instant endTime = Instant.now(); // 35 ms
+		System.out.println("updateCourse() method finished at : " + endTime);
+		System.out.println("updateCourse() method time elapsed : " + Duration.between(startTime, endTime));
+
+		// send updated object
+		return upadtedCourse;
+	}
+
+	public Course updateCourseTopicByTopicId(Long courseId, Long topicId, Topic topic)
+	{
+		// TimeStamp Before The Actual Operation  (@Before)
+		Instant startTime = Instant.now();
+		System.out.println("updateCourseTopicByTopicId() method started at : " + startTime);
+
+		// Starts Here Code
+		Course updatedTopicsCourse = null;
+		// Fetch The Course With courseId
+		Optional<Course> courseOptional = courseRepository.findById(courseId);
+
+		if(courseOptional.isPresent())
+		{
+			updatedTopicsCourse = courseOptional.get();
+
+			for(Topic target :  updatedTopicsCourse.getTopics())
+			{
+				if(target.getTopicId().equals(topicId))
+				{
+					if(topic.getTitle().isEmpty())
+					{
+						target.setTitle(topic.getTitle());
+					}
+
+					if(topic.getDescription().isEmpty())
+					{
+						target.setDescription(topic.getDescription());
+					}
+
+					break;
+				}
+
+			}
+			updatedTopicsCourse =   courseRepository.save(updatedTopicsCourse);
+
+		}
+
+		// TimeStamp After The Actual Operation (@After)
+		Instant endTime = Instant.now(); // 35 ms
+		System.out.println("updateCourseTopicByTopicId() method finished at : " + endTime);
+		System.out.println("updateCourseTopicByTopicId() method time elapsed : " + Duration.between(startTime, endTime));
+
+		return updatedTopicsCourse;
 	}
 }
